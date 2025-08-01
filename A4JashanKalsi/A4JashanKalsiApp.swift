958f89f6abf76dde62f918770915b2b6050b1a72
Jashan <jashanjot381@gmail.com>
2025-08-01 06:31:21 -0400

    Initial Commit

---
+++ A4JashanKalsi/A4JashanKalsiApp.swift
@@ -0,0 +1,17 @@
+//
+//  A4JashanKalsiApp.swift
+//  A4JashanKalsi
+//
+//  Created by Jashan Kalsi on 2025-08-01.
+//
+
+import SwiftUI
+
+@main
+struct A4JashanKalsiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A4JashanKalsi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A4JashanKalsi
+//
+//  Created by Jashan Kalsi on 2025-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

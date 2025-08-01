8a031923c70ece8245094d5e21542af0e450ad95
Jashan <jashanjot381@gmail.com>
2025-08-01 09:26:21 -0400

    Added images

---
+++ A4JashanKalsi/Views/ToDoItemRow.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoItemRow.swift
+//  A4JashanKalsi
+//
+//  Created by Jashan Kalsi on 2025-08-01.
+//
+
+import SwiftUI
+
+struct ToDoItemRow: View {
+    let toDo: ToDo
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoItemRow(toDo: ToDo.sampleData[0])
+}

99f88b09332dc5a056aa2a141b8586a78b335f69
Jashan <jashanjot381@gmail.com>
2025-08-04 18:24:31 -0400

    List View logic testing

--- A4JashanKalsi/Model/ToDo.swift
+++ A4JashanKalsi/Model/ToDo.swift
@@ -28,9 +28,9 @@ struct ToDo: Identifiable{
     // Static list of hardcoded data for the app
     static var sampleData: [ToDo] = [
         // Create specific dates using DateComponents
-        ToDo(id: 0, title: "Buy milk", priority: .critical, deadline: createDate(year: 2025, month: 8, day: 2, hour: 9, minute: 41), description: "Remember to buy milk"),
-        ToDo(id: 1, title: "Finish project", priority: .important, deadline: createDate(year: 2025, month: 8, day: 5, hour: 12, minute: 00), description: "Make sure to finish the project"),
-        ToDo(id: 2, title: "Call Bank", priority: .regular, deadline: createDate(year: 2025, month: 8, day: 2, hour: 9, minute: 30), description: "GIC details"),
+        ToDo(id: 0, title: "Buy milk", priority: .critical, deadline: createDate(year: 2025, month: 8, day: 12, hour: 11, minute: 55), description: "Remember to buy milk"),
+        ToDo(id: 1, title: "Finish the project", priority: .important, deadline: createDate(year: 2025, month: 8, day: 24, hour: 12, minute: 00), description: "Make sure to finish the project"),
+        ToDo(id: 2, title: "Call Bank", priority: .regular, deadline: createDate(year: 2025, month: 8, day: 11, hour: 9, minute: 30), description: "GIC details"),
         ToDo(id: 3, title: "Assignment INFO47546", priority: .critical, deadline: createDate(year: 2025, month: 7, day: 30, hour: 11, minute: 59), description: "Assignment 02 completion"),
         ]
 }
---
+++ A4JashanKalsi/Views/ToDoListView.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoListView.swift
+//  A4JashanKalsi
+//
+//  Created by Jashan Kalsi on 2025-08-04.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    // Access the static list of tasks from the ToDo model
+    let tasks: [ToDo] = ToDo.sampleData
+    
+    var body: some View {
+        NavigationView {
+            List(tasks) { task in
+                // Each item in the list is a link to the detail view
+                NavigationLink(destination: ToDoItemRow(ToDo: ToDo)) {
+                    ToDoItemRow(ToDo: ToDo)
+                }
+                Text(task.title)
+            }
+            .navigationBarTitle("To-Do List")
+        }
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
